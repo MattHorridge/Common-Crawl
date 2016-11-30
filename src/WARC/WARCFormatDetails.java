e6238cf2af0e1b16b0af36547b08ca1c7b57e8f2
Matt Horridge <matthorridge@matts-air.home>
2016-11-30 21:56:31 +0000

    [WIP] Added Record Object and Details in. Needs Attention: TODO: Parse List, store in MAP, GenerateContentBlock

--- src/WARC/WARCFormatDetails.java
+++ src/WARC/WARCFormatDetails.java
@@ -1,11 +1,49 @@
 package WARC;
 
+import java.util.regex.*;
+
 public interface WARCFormatDetails {
 	
 	
 	//Interface to help handle HTTP responses in WARC file
 	
 	
+	
+	String regexblank = "^\\s";
+	
+	//Responses are only relevant to this project
+	enum WARCType{
+		warcinfo,
+		request,
+		response,
+		metadata
+	
+	}
+	
+	
+	
+	
+	
+	//Array of all Header Fields in a WARC Record
+	public static String[] HeaderFields = {
+			"WARC-Type",
+			"WARC-Date",
+			"WARC-Record-ID",
+			"Content-Length",
+			"Content-Type",
+			"WARC-Concurrent-To ",
+			"WARC-Block-Digest ",
+			"WARC-Payload-Digest",
+			"WARC-IP-Address",
+			"WARC-Refers-To",
+			"WARC-Target-URI ",
+			"WARC-Truncated",
+			"WARC-Warcinfo-ID",
+			"WARC-Filename", //warcinfo only
+			"WARC-Identified-Payload-Type"
+			
+	};
+		
 	
 
 }
--- src/WARC/WARCRecord.java
+++ src/WARC/WARCRecord.java
@@ -1,14 +1,72 @@
 package WARC;
 
+import java.io.InputStream;
+import java.util.List;
+
 public class WARCRecord implements WARCFormatDetails {
 	
+
+	protected String type;
+	protected int ContentLength;
+	protected String TargetURI;
+	protected List ContentBlock;
+	
+	//Need to get Content block
+
 	
 	
-	
+	//blank constructor
 	public WARCRecord(){
 		
 		
 	}
+	
+	
+	public WARCRecord(InputStream in){
+		
+	}
+	
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public int getContentLength() {
+		return ContentLength;
+	}
+
+
+	public void setContentLength(int contentLength) {
+		ContentLength = contentLength;
+	}
+
+
+	public String getTargetURI() {
+		return TargetURI;
+	}
+
+
+	public void setTargetURI(String targetURI) {
+		TargetURI = targetURI;
+	}
+
+
+	public List getContentBlock() {
+		return ContentBlock;
+	}
+
+
+
+	
+
+	
+	
 	
 	
 	
