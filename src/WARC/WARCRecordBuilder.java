fd8b8d9ba8c7aa28507963971010c2589bd17d69
Matt Horridge <matthorridge@matts-air.home>
2016-12-01 21:20:21 +0000

    minor changes}

---
+++ src/WARC/WARCRecordBuilder.java
@@ -0,0 +1,16 @@
+package WARC;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class WARCRecordBuilder implements WARCFormatDetails{
+
+	
+	
+	
+	
+	
+
+}
