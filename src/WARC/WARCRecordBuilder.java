8e6197fa8ba03861cf188341b6a71ee097a25256
Matt Horridge <matthorridge@matts-air.home>
2016-12-01 21:21:13 +0000

    minor changes

--- src/WARC/WARCRecordBuilder.java
+++ src/WARC/WARCRecordBuilder.java
@@ -1,12 +1,48 @@
 package WARC;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 
+/**
+ * 
+ * @author matthorridge
+ * Main Class to build Response WARC Records
+ * Implements WARC Details for parsing
+ */
+
+
 public class WARCRecordBuilder implements WARCFormatDetails{
 
+	
+	
+	File file;
+	InputStream in;
+	
+	
+	/**
+	 * Constructor
+	 */
+	
+	public WARCRecordBuilder(InputStream streamIn, File fileIn){
+		
+		in = streamIn;
+		file = fileIn;
+		
+	}
+	
+	
+	
+	public WARCRecord buildRecord(){
+		
+		
+		return null;
+	}
+	
+	
+	
 	
 	
 	
